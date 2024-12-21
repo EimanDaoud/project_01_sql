b113b94fafaccb65577cdb0d185829bd2fe9fa88
Eiman Daoud <eimandaoud4@gmail.com>
2024-12-22 01:21:50 +0200

    upload sql files

--- Project_SQL/3_top_demanded_skills.sql
+++ Project_SQL/3_top_demanded_skills.sql
@@ -2,8 +2,17 @@
 
 
 
-SELECT * 
+SELECT
+    skills,
+    COUNT(s.job_id) AS demand_count
 FROM job_postings_fact jpf
 INNER JOIN skills_job_dim s ON jpf.job_id = s.job_id
 INNER JOIN skills_dim d ON s.skill_id = d.skill_id
-LIMIT 5
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_work_from_home = TRUE
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5;
